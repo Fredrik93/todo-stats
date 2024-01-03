f9f353a1d642b6c8db8957bf6867da968ddb2ae3
Fredrik93 <fredrikullman93@gmail.com>
2024-01-03 19:47:50 +0100

    update item

--- Components/Todo.js
+++ Components/Todo.js
@@ -1,18 +1,31 @@
-import React, { Fragment } from 'react'
-import { StyleSheet, Text, Image, TextBase } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 
 const Todo = ({ title }) => {
-
     return (
-        <Text > {title}</Text>
+        <View style={styles.todoItem}>
+            <CheckBox />
+            <Text style={styles.title}>{title}</Text>
 
-    )
-}
+            {/* You can add checkbox and delete button components here */}
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-
-
-})
+    todoItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+    },
+    title: {
+        flex: 1,
+        fontSize: 18,
+    },
+    // Add styles for checkbox and delete button here
+});
 
 export default Todo;
